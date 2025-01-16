ad03f8bdbd8aa99a61bc78e82cde027d03339c40
Roberto Paulino <rpaulinodev@gmail.com>
2025-01-15 23:43:14 -0500

    More code for the config.

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,21 +7,54 @@ import (
 	"os"
 )
 
+const configFileName = ".blog-aggregatorconfig.json"
+
 type Config struct{
   DbUrl string `json:"db_url"`
   CurrentUser string `json:"current_user_name"`
 }
 
 
+func (c *Config) SetUser() {
+  
+}
+
+func getConfigFilePath() (string, error){
+
+  homeDir, err := os.UserHomeDir()
+  if err != nil {
+    return "", err
+  }
+  configLocation := fmt.Sprintf("%v%v", homeDir, configFileName)
+
+  return configLocation, nil
+}
+
+func write(conf Config) error {
+  // conf -> json | get conf location in home directory | write new json to that location
+  configPath, err := getConfigFilePath()
+  if err != nil {
+    return err 
+  }
+
+  jsonData, err := json.Marshal(conf)
+  if err != nil {
+    return err
+  }
+  
+
+  
+  return nil
+}
+
 func Read() (Config, error){
 
   conf := Config {}
 
-  homeDir, err := os.UserHomeDir()
+  configLocation, err := getConfigFilePath()
   if err != nil {
     return conf, err
   }
-  configLocation := fmt.Sprintf("%v/blog-aggregatorconfig.json", homeDir)
 
   jsonFile, err := os.Open(configLocation)
   if err != nil {
